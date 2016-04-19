e9eb07eb0892965001d86d052b63b91646eb63de
Eric Li <li.eric42@gmail.com>
2016-04-19 15:02:03 +0800

    Edited formatting

--- SmallestCommonMultiple.js
+++ SmallestCommonMultiple.js
@@ -1,16 +1,18 @@
 function smallestCommons(arr) {
-  for (i = Math.max(...arr); i >= Math.min(...arr); i--) { arr.push(i); }
+  for (i = Math.max(...arr); i >= Math.min(...arr); i--) { 
+    arr.push(i); 
+  }
 
-    return arr.slice(2).reduce(function(x, y) {
-      var a = x, b = y, t = 0;
-      while (a % b) {
-        a = a % b; 
-        t = a; 
-        a = b; 
-        b = t; 
-      }
-      return x / b * y;
-    });
+  return arr.slice(2).reduce(function(x, y) {
+    var a = x, b = y, t = 0;
+    while (a % b) {
+      a = a % b;
+      t = a;
+      a = b;
+      b = t;
+    }
+    return x / b * y;
+  });
 }
 
 smallestCommons([1, 25]);
