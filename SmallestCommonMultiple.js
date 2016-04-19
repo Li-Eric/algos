2f81504036c7739b5bd3b5d49aa6b7996a52bd1f
Eric Li <li.eric42@gmail.com>
2016-04-19 17:15:57 +0800

    Updated answers

--- SmallestCommonMultiple.js
+++ SmallestCommonMultiple.js
@@ -1,18 +1,21 @@
 function smallestCommons(arr) {
-  for (i = Math.max(...arr); i >= Math.min(...arr); i--) { 
-    arr.push(i); 
-  }
 
-  return arr.slice(2).reduce(function(x, y) {
-    var a = x, b = y, t = 0;
-    while (a % b) {
-      a = a % b;
-      t = a;
-      a = b;
-      b = t;
-    }
-    return x / b * y;
-  });
+	// Source: http://stackoverflow.com/questions/35073593/javascript-code-for-lowest-common-multiple
+	// Based on the euclidean algorithm for finding primes
+	for (i = Math.max(...arr); i >= Math.min(...arr); i--) { 
+		arr.push(i); 
+	}
+
+	return arr.slice(2).reduce(function(x, y) {
+		var a = x, b = y, t = 0;
+		while (a % b) {
+			a = a % b;
+			t = a;
+			a = b;
+			b = t;
+		}
+		return x / b * y;
+	});
 }
 
 smallestCommons([1, 25]);
--- SumAllPrimes.js
+++ SumAllPrimes.js
@@ -3,6 +3,7 @@ function sumPrimes(max) {
   var sum = 0;
 
   // Use the Sieve of Eratosthenes to find primes
+  // Sieve of Eratosthenes implementation from: http://stackoverflow.com/questions/11966520/how-to-find-prime-numbers-between-0-100
   for (i = 2; i <= max; i++) {
     if (!arr[i]) {
       // i has not been marked -- it is prime
